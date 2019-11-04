58b1a8a79cb32e7c1017de24bc2e4ee63cf62a9a
hongzhonglu <luho@chalmers.se>
2019-11-04 14:45:03 +0100

    fix: update some script

---
+++ refine draft strain specific GEM for 332 yeast species/Change_txt_into_tsv.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# -*- python 3 -*-
+# -*- hongzhong Lu -*-
+
+import os
+import csv
+os.chdir('/Users/luho/PycharmProjects/3D_model/refine draft strain specific GEM for 332 yeast species')
+
+
+
+strain = os.listdir('/Users/luho/PycharmProjects/3D_model/refine draft strain specific GEM for 332 yeast species/strain specific model from RAVEN_biocyc_55_110/')
+strain = [x for x in strain if x != '.DS_Store']
+for x in strain:
+    print(x)
+    data = open('/Users/luho/PycharmProjects/3D_model/refine draft strain specific GEM for 332 yeast species/strain specific model from RAVEN_biocyc_55_110/' + x +'/excelRxns.txt', 'r').readlines()
+    with open('/Users/luho/PycharmProjects/3D_model/refine draft strain specific GEM for 332 yeast species/strain specific model from RAVEN_biocyc_55_110/' + x +'/draft_GEM.tsv', mode='w') as file:
+        employee_writer = csv.writer(file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for i, line in enumerate(data):
+            line = line.replace("#", "").lstrip()
+            line = line.strip('\n')
+            line = line.strip('\t')
+            #if i != 0:
+            line0 = line.split("\t")
+            print(line0)
+            employee_writer.writerow(line0)
+
+
+'''
+# remove file using python code
+my_dir = "/Users/luho/PycharmProjects/3D_model/refine draft strain specific GEM for 332 yeast species/strain specific model from RAVEN_biocyc_55_110/"
+for fname in os.listdir(my_dir):
+    print(fname)
+    if fname !='.DS_Store':
+        newfname = os.path.join(my_dir, fname)
+    for name2 in os.listdir(newfname):
+        print(name2)
+        if name2.startswith("csv"):
+            print(name2)
+            os.remove(os.path.join(newfname, name2))
+'''
+
+strain = os.listdir('/Users/luho/PycharmProjects/3D_model/refine draft strain specific GEM for 332 yeast species/strain specific model from RAVEN_kegg/')
+strain = [x for x in strain if x != '.DS_Store']
+for x in strain:
+    print(x)
+    data = open('/Users/luho/PycharmProjects/3D_model/refine draft strain specific GEM for 332 yeast species/strain specific model from RAVEN_kegg/' + x +'/excelRxns.txt', 'r').readlines()
+    with open('/Users/luho/PycharmProjects/3D_model/refine draft strain specific GEM for 332 yeast species/strain specific model from RAVEN_kegg/' + x +'/draft_GEM.tsv', mode='w') as file:
+        employee_writer = csv.writer(file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for i, line in enumerate(data):
+            line = line.replace("#", "").lstrip()
+            line = line.strip('\n')
+            line = line.strip('\t')
+            #if i != 0:
+            line0 = line.split("\t")
+            print(line0)
+            line0 = line0[0:11]
+            employee_writer.writerow(line0)
--- refine draft strain specific GEM for 332 yeast species/draft_strain_specific_model_analysis.R
+++ refine draft strain specific GEM for 332 yeast species/draft_strain_specific_model_analysis.R
@@ -122,3 +122,49 @@ plotXYdotGraph2(data_frame=summary_paramter2, paraX = 'gene', paraY = 'rxn', xla
 plotXYdotGraph2(data_frame=summary_paramter2, paraX = 'gene_set', paraY = 'rxn', xlab_name='Gene set', ylab_name='rxn number_kegg')
 plotXYdotGraph2(data_frame=summary_paramter2, paraX = 'gene_set', paraY = 'gene', xlab_name='Gene set', ylab_name='Metabolic gene_kegg')
 
+
+
+
+
+
+
+#---------------------------------------------------
+## Small task- to check whether the specific reactions existing in all yeast species
+# RAVEN kegg
+gene_all <- vector()
+rxn_all <- vector()
+exist_R01867 <- vector()
+ec <- c('ec:1.3.98.1','ec:1.3.5.2')#,'ec:1.3.1.14')
+ec_all <- vector()
+strain <- list.files('strain specific model from RAVEN_kegg')
+ec_rxn <- read.table('data/EC_rxn_mapping_kegg.txt', stringsAsFactors = FALSE) # ec rxn mapping from kegg
+ec_rxn$V2 <- str_replace_all(ec_rxn$V2, "rn:", "")
+
+
+for (i in strain) {
+  print(i)
+  #i <- "biocyc_panYeast2_45_100"
+  inputfile <- paste('strain specific model from RAVEN_kegg/',i, '/excelGenes.txt', sep = "")
+  inputfile2 <- paste('strain specific model from RAVEN_kegg/',i, '/excelRxns.txt', sep = "")
+  
+  gene_biocyc <- read.table(inputfile, header =FALSE, sep = "\t", stringsAsFactors = FALSE)
+  gene <- length(unique(gene_biocyc$V2))
+  
+  rxn_biocyc <- read_table2(inputfile2)
+  rxn <- length(unique(rxn_biocyc$`#`))
+  
+  rxn_detail <- data_frame(rxn=rxn_biocyc$`#`)
+  rxn_detail <- merge(rxn_detail, ec_rxn, by.x = 'rxn', by.y = 'V2', all.x = TRUE)
+  existence <- length(which(rxn_detail$rxn %in% 'R01867'))
+  
+
+  ec_existence <- length(which(rxn_detail$V1 %in% ec))
+  #save the result
+  exist_R01867 <- c(exist_R01867, existence)
+  ec_all <- c(ec_all, ec_existence)
+  gene_all <- c(gene_all, gene)
+  rxn_all <- c(rxn_all, rxn)
+}
+
+
+
