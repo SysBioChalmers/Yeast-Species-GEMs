19bbc023f8074ee7ae540a5a8613fd5b5be70b3c
hongzhonglu <luho@chalmers.se>
2019-11-07 13:17:18 +0100

    feat: add model file for 11 out-group strains

--- draft_GEM_all_yeast_species/Change_txt_into_tsv.py
+++ draft_GEM_all_yeast_species/Change_txt_into_tsv.py
@@ -4,16 +4,16 @@
 
 import os
 import csv
-os.chdir('/Users/luho/PycharmProjects/3D_model/refine draft strain specific GEM for 332 yeast species')
+os.chdir('/Users/luho/Documents/GitHub/Pan_Model_332_Yeast_Species/draft_GEM_all_yeast_species')
 
 
 
-strain = os.listdir('/Users/luho/PycharmProjects/3D_model/refine draft strain specific GEM for 332 yeast species/strain specific model from RAVEN_biocyc_55_110/')
+strain = os.listdir('/Users/luho/Documents/GitHub/Pan_Model_332_Yeast_Species/draft_GEM_all_yeast_species/strain specific model from RAVEN_biocyc_55_110/')
 strain = [x for x in strain if x != '.DS_Store']
 for x in strain:
     print(x)
-    data = open('/Users/luho/PycharmProjects/3D_model/refine draft strain specific GEM for 332 yeast species/strain specific model from RAVEN_biocyc_55_110/' + x +'/excelRxns.txt', 'r').readlines()
-    with open('/Users/luho/PycharmProjects/3D_model/refine draft strain specific GEM for 332 yeast species/strain specific model from RAVEN_biocyc_55_110/' + x +'/draft_GEM.tsv', mode='w') as file:
+    data = open('/Users/luho/Documents/GitHub/Pan_Model_332_Yeast_Species/draft_GEM_all_yeast_species/strain specific model from RAVEN_biocyc_55_110/' + x +'/excelRxns.txt', 'r').readlines()
+    with open('/Users/luho/Documents/GitHub/Pan_Model_332_Yeast_Species/draft_GEM_all_yeast_species/strain specific model from RAVEN_biocyc_55_110/' + x +'/draft_GEM.tsv', mode='w') as file:
         employee_writer = csv.writer(file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         for i, line in enumerate(data):
             line = line.replace("#", "").lstrip()
@@ -39,12 +39,12 @@ for fname in os.listdir(my_dir):
             os.remove(os.path.join(newfname, name2))
 '''
 
-strain = os.listdir('/Users/luho/PycharmProjects/3D_model/refine draft strain specific GEM for 332 yeast species/strain specific model from RAVEN_kegg/')
+strain = os.listdir('/Users/luho/Documents/GitHub/Pan_Model_332_Yeast_Species/draft_GEM_all_yeast_species/strain specific model from RAVEN_kegg/')
 strain = [x for x in strain if x != '.DS_Store']
 for x in strain:
     print(x)
-    data = open('/Users/luho/PycharmProjects/3D_model/refine draft strain specific GEM for 332 yeast species/strain specific model from RAVEN_kegg/' + x +'/excelRxns.txt', 'r').readlines()
-    with open('/Users/luho/PycharmProjects/3D_model/refine draft strain specific GEM for 332 yeast species/strain specific model from RAVEN_kegg/' + x +'/draft_GEM.tsv', mode='w') as file:
+    data = open('/Users/luho/Documents/GitHub/Pan_Model_332_Yeast_Species/draft_GEM_all_yeast_species/strain specific model from RAVEN_kegg/' + x +'/excelRxns.txt', 'r').readlines()
+    with open('/Users/luho/Documents/GitHub/Pan_Model_332_Yeast_Species/draft_GEM_all_yeast_species/strain specific model from RAVEN_kegg/' + x +'/draft_GEM.tsv', mode='w') as file:
         employee_writer = csv.writer(file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         for i, line in enumerate(data):
             line = line.replace("#", "").lstrip()
