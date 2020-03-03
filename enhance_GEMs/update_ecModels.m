49e9ebf0dd0f20d99b673396d61fca4bf5ab107f
Ivan Domenzain <ivand@chalmers.se>
2020-03-03 17:59:45 +0100

    feat: add script for enhancing yeasts species models with enzyme constraints

---
+++ enhance_GEMs/update_ecModels.m
@@ -0,0 +1,39 @@
+%Clone GECKO repository
+git('clone https://github.com/SysBioChalmers/GECKO.git')
+cd GECKO
+git('pull')
+%Locate the correct branch
+git('checkout fix/updateDatabases') 
+clc
+fileNames = dir('../models');
+%Upload KEGG codes info
+fID        = fopen('../../ComplementaryData/yeasts_keggCodes.txt');
+keggCodes  = textscan(fID,'%s %s %s','Delimiter','\t','HeaderLines',1);
+variables  = {'strain','modelName','KEGG'};
+keggCodes = table(keggCodes{1},keggCodes{2},keggCodes{3},'VariableNames', variables);
+%Substitute some scripts in GECKO
+copyfile('../updateDatabases.m','geckomat/get_enzyme_data/updateDatabases.m')
+current = pwd;
+for i=1:length(fileNames)
+    cd(current)
+    file = fileNames(i).name;
+    if contains(file,'.mat')
+        disp(file)
+        modelName = file(1:(end-4));
+        disp(modelName)
+        DBname    = ['../../ComplementaryData/databases/uniprot/' modelName '.tab'];
+        %If a uniprot file is available for this organism in the repository
+        %then paste it in GECKO
+        if isfile(DBname)
+            copyfile(DBname,'databases/uniprot.tab')
+            index = find(strcmpi(keggCodes.modelName,modelName),1);
+            keggCode = keggCodes.KEGG(index);
+            disp(keggCode)
+            %Generate protDatabase (uniprot and KEGG for the desired
+            %organism)
+            cd geckomat/get_enzyme_data
+            updateDatabases(keggCode{1});
+            copyfile('../../Databases/protDatabase.mat',['../../Databases/' modelName '_protDatabase.mat'])
+        end
+    end
+end
