055d868aae528354f835d2578e580e16bd3999f5
hongzhonglu <31989005+hongzhonglu@users.noreply.github.com>
2022-06-12 16:00:12 +0800

    fix: remove used files

--- draft_GEM_all_yeast_species/remove_unused_files.py
+++ draft_GEM_all_yeast_species/remove_unused_files.py
@@ -21,6 +21,19 @@ for fname in strain:
             print(name2)
             os.remove(os.path.join(newfname, name2))
 
+for fname in strain:
+    print(fname)
+    if fname !='.DS_Store':
+        newfname = os.path.join(my_dir, fname)
+    for name2 in os.listdir(newfname):
+        print(name2)
+        if name2.startswith("excelModel"):
+            print(name2)
+            os.remove(os.path.join(newfname, name2))
+
+
+
+
 
 my_dir = os.path.join(dir0, "strain_specific_model_from_RAVEN_kegg/")
 strain = os.listdir(my_dir)
@@ -36,3 +49,12 @@ for fname in strain:
             print(name2)
             os.remove(os.path.join(newfname, name2))
 
+for fname in strain:
+    print(fname)
+    if fname !='.DS_Store':
+        newfname = os.path.join(my_dir, fname)
+    for name2 in os.listdir(newfname):
+        print(name2)
+        if name2.startswith("excelModel"):
+            print(name2)
+            os.remove(os.path.join(newfname, name2))
